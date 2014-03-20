5ec23789c8774f65704eefb3233a2289ce05a48d
KryptykFysh <kryptykfysh@kryptykfysh.co.uk>
2014-03-20 21:02:44 +1100

    Logging moves to its own module.

--- lib/ruby_quest.rb
+++ lib/ruby_quest.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 
+require_relative 'logging'
 require_relative 'ruby_quest/database'
 require_relative 'ruby_quest/server'
 require_relative 'ruby_quest/character'
-require_relative 'ruby_quest/logging'
